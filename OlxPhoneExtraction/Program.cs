adc4f99e256c5a87f4f8be5172f1a5df8d16d5fe
Pedro Henrique Perim Franco Neiva <pedroh.franco@outlook.com>
2019-08-24 13:37:09 -0300

    Adicionado Library de consulta

---
+++ OlxPhoneExtraction/Program.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+
+namespace OlxPhoneExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new HtmlWeb();
+            var doc = web.Load("https://go.olx.com.br/grande-goiania-e-anapolis/regiao-norte");
+
+            var nextPage = doc.DocumentNode.SelectSingleNode("//li[contains(@class, 'item next')]/a");
+
+            while(nextPage != null)
+            {
+                var items = doc.DocumentNode.SelectNodes("//a[contains(@class, 'OLXad-list-link ')]");
+                foreach (var item in items)
+                {
+                    var webAdPage = new HtmlWeb();
+                    var docAdPage = webAdPage.Load(item.GetAttributeValue("href", ""));
+                }
+
+                doc = web.Load(nextPage.GetAttributeValue("href", ""));
+                nextPage = doc.DocumentNode.SelectSingleNode("//li[contains(@class, 'item next')]/a");
+            }
+        }
+    }
+}
