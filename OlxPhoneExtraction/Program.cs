6bf83b1cc72b5b027e1a0ac5cdf1867bc319133c
Pedro Henrique Franco <pedroh.franco@outlook.com>
2019-08-25 23:06:06 -0300

    Altrações para obter pagina de post

--- OlxPhoneExtraction/Program.cs
+++ OlxPhoneExtraction/Program.cs
@@ -1,12 +1,15 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace OlxPhoneExtraction
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var web = new HtmlWeb();
             var doc = web.Load("https://go.olx.com.br/grande-goiania-e-anapolis/regiao-norte");
@@ -15,16 +18,27 @@ namespace OlxPhoneExtraction
 
             while(nextPage != null)
             {
-                var items = doc.DocumentNode.SelectNodes("//a[contains(@class, 'OLXad-list-link ')]");
-                foreach (var item in items)
+                foreach (var item in doc.DocumentNode.SelectNodes("//a[contains(@class, 'OLXad-list-link ')]"))
                 {
                     var webAdPage = new HtmlWeb();
-                    var docAdPage = webAdPage.Load(item.GetAttributeValue("href", ""));
+                    var docAdPage = await LoadHtmlWithBrowser(item.GetAttributeValue("href", "")).ConfigureAwait(false);
+                    var spanNumber = docAdPage.DocumentNode.SelectNodes("//span[contains(@class, 'lhsaj')]");
                 }
 
                 doc = web.Load(nextPage.GetAttributeValue("href", ""));
                 nextPage = doc.DocumentNode.SelectSingleNode("//li[contains(@class, 'item next')]/a");
             }
+        }
+
+        private static async Task<HtmlDocument> LoadHtmlWithBrowser(String url)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url).ConfigureAwait(false);
+            response.EnsureSuccessStatusCode();
+            var pageContents = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+            return pageDocument;
         }
     }
 }
